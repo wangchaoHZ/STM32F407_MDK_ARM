2a5ec9cd407e35c0e6ebf5ab6d74ade687ae5b1b
wangchaoHZ <2399264862@qq.com>
2022-09-26 07:28:47 +0800

    init .

---
+++ Application/main.c
@@ -0,0 +1,24 @@
+/*
+ * @Author       : wang chao
+ * @Date         : 2022-09-23 15:53:34
+ * @LastEditors  : wang chao
+ * @LastEditTime : 2022-09-26 07:24:55
+ * @FilePath     : main.c
+ * @Description  : 
+ * Copyright 2022 BingShan, All Rights Reserved. 
+ */
+
+#include "main.h"
+
+/**
+ * @brief  Main program
+ * @param  None
+ * @retval None
+ */
+int main(void)
+{
+    /* Infinite loop */
+    while (1)
+    {
+    }
+}
---
+++ Application/main.h
@@ -0,0 +1,20 @@
+/*
+ * @Author       : wang chao
+ * @Date         : 2022-09-23 15:53:34
+ * @LastEditors  : wang chao
+ * @LastEditTime : 2022-09-26 07:26:28
+ * @FilePath     : main.h
+ * @Description  : 
+ * Copyright 2022 BingShan, All Rights Reserved. 
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+
+
+#endif /* __MAIN_H */
+
